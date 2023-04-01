ac6401daafc303e3a02863def8284bf275d12b66
kamack38 <64226248+kamack38@users.noreply.github.com>
2023-04-01 19:56:23 +0200

    refactor: 💡 Refactor repo

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer({
+const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-})
+}
+
+module.exports = nextConfig
